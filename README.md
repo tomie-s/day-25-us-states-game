7824ebc12755e60ae5ac93c9e5bcb9d98685606b
Tomi Salami <salami.tomie@gmail.com>
2024-06-16 22:10:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# day-25-us-states-game
+ US States Games that allows users to guess the names of states.
---
+++ main.py
@@ -0,0 +1,39 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title('U.S. States Game')
+
+image = 'blank_states_img.gif'
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv('50_states.csv')
+all_states = data.state.to_list()
+
+correct_states = []
+
+while len(correct_states) < 50:
+    answer_state = screen.textinput(title=f"{len(correct_states)}/50 States Correct",
+                                    prompt='What is another State name?').title()
+
+    if answer_state == 'Exit':
+        missing_states = []
+
+        for state in all_states:
+            if state not in correct_states:
+                missing_states.append(state)
+
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv('states_to_learn.csv')
+        break
+
+    if answer_state in all_states:
+        correct_states.append(answer_state)
+        state_pos = data[data['state'] == answer_state].iloc[0]
+
+        pen = turtle.Turtle()
+        pen.hideturtle()
+        pen.penup()
+        pen.goto(state_pos.x, state_pos.y)
+        pen.write(answer_state)
